ee314ae241b52ee57f695df27cc4784e755929af
Grace Mora <autobots00019@gmail.com>
2024-06-13 03:48:49 +0800

    chore: dynamically render body based on headerType;

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -3,30 +3,34 @@ import Header from '../header/Header';
 import { useState } from "react";
 import CalendarGrid from './calendar-grid/CalendarGrid';
 import MonthView from '../month-view/MonthView';
-
-const daysHeader = ['Su','Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+import YearView from '../year-view/YearView';
 
 function Calendar() {
   const currentMonth = new Date().getMonth();
   const currentYear = new Date().getFullYear();
   const [selectedMonth, setSelectedMonth] = useState(currentMonth);
   const [selectedYear, setSelectedYear] = useState(currentYear);
+  const [headerType, setHeaderType] = useState(1);
+
+  function renderBody () {
+    switch (headerType) {
+      case 2:
+        return (<MonthView monthState={{ selectedMonth, setSelectedMonth }} typeState={{headerType, setHeaderType}}></MonthView>)
+      case 3:
+        return (<YearView yearState={{ selectedYear, setSelectedYear }} typeState={{headerType, setHeaderType}}></YearView>)
+      default:
+        return (<CalendarGrid monthState={{ selectedMonth, setSelectedMonth }} yearState={{ selectedYear, setSelectedYear }}></CalendarGrid>);
+    } 
+  }
 
   return (
     <div className="calendar">
-      <Header month={selectedMonth} year={selectedYear} modifyMonth={setSelectedMonth} modifyYear={setSelectedYear}></Header>
-      <div className="row">
-        {daysHeader.map((day,index) => {
-          return <div key={index} className="name">{day}</div>
-        })}
-      </div>
+      <Header monthState={{ selectedMonth, setSelectedMonth }} yearState={{ selectedYear, setSelectedYear }} typeState={{headerType, setHeaderType}}></Header>
       <div className="body">
-        <CalendarGrid month={selectedMonth} year={selectedYear} modifyMonth={setSelectedMonth} modifyYear={setSelectedYear}></CalendarGrid>
-        <MonthView monthState={{selectedMonth, setSelectedMonth}}></MonthView>
+        {renderBody()}
       </div>
     </div>
   )
-    
 }
 
 export default Calendar;
