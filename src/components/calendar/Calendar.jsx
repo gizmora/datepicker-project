be743942bd17ecacaa6bdeb1793c11f1d5ee364a
Grace Mora <50357533+gizmora@users.noreply.github.com>
2024-06-13 04:30:05 +0800

    chore: cleanup

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,9 +1,9 @@
-import './Calendar.css';
-import Header from '../header/Header';
+import "./Calendar.css";
+import Header from "../header/Header";
 import { useState } from "react";
-import CalendarGrid from './calendar-grid/CalendarGrid';
-import MonthView from '../month-view/MonthView';
-import YearView from '../year-view/YearView';
+import CalendarGrid from "./calendar-grid/CalendarGrid";
+import MonthView from "../month-view/MonthView";
+import YearView from "../year-view/YearView";
 
 function Calendar() {
   const currentMonth = new Date().getMonth();
@@ -12,25 +12,42 @@ function Calendar() {
   const [selectedYear, setSelectedYear] = useState(currentYear);
   const [headerType, setHeaderType] = useState(1);
 
-  function renderBody () {
+  function renderBody() {
     switch (headerType) {
       case 2:
-        return (<MonthView monthState={{ selectedMonth, setSelectedMonth }} typeState={{headerType, setHeaderType}}></MonthView>)
+        return (
+          <MonthView
+            monthState={{ selectedMonth, setSelectedMonth }}
+            typeState={{ headerType, setHeaderType }}
+          />
+        );
       case 3:
-        return (<YearView yearState={{ selectedYear, setSelectedYear }} typeState={{headerType, setHeaderType}}></YearView>)
+        return (
+          <YearView
+            yearState={{ selectedYear, setSelectedYear }}
+            typeState={{ headerType, setHeaderType }}
+          />
+        );
       default:
-        return (<CalendarGrid monthState={{ selectedMonth, setSelectedMonth }} yearState={{ selectedYear, setSelectedYear }}></CalendarGrid>);
-    } 
+        return (
+          <CalendarGrid
+            monthState={{ selectedMonth, setSelectedMonth }}
+            yearState={{ selectedYear, setSelectedYear }}
+          />
+        );
+    }
   }
 
   return (
     <div className="calendar">
-      <Header monthState={{ selectedMonth, setSelectedMonth }} yearState={{ selectedYear, setSelectedYear }} typeState={{headerType, setHeaderType}}></Header>
-      <div className="body">
-        {renderBody()}
-      </div>
+      <Header
+        monthState={{ selectedMonth, setSelectedMonth }}
+        yearState={{ selectedYear, setSelectedYear }}
+        typeState={{ headerType, setHeaderType }}
+      />
+      <div className="body">{renderBody()}</div>
     </div>
-  )
+  );
 }
 
 export default Calendar;
