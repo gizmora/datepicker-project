aedf0b92f13d2c24dcf32d2c6321af150b3090e5
Grace Mora <autobots00019@gmail.com>
2024-06-13 03:46:50 +0800

    chore: added year view

---
+++ src/components/year-view/YearView.jsx
@@ -0,0 +1,52 @@
+import PropTypes from 'prop-types';
+import './YearView.css';
+
+function YearView({ yearState, typeState }) {
+  const { selectedYear, setSelectedYear } = yearState;
+  const { setHeaderType } = typeState;
+
+  function clickYearHandler(year) {
+    setSelectedYear(year);
+    setHeaderType(2);
+  }
+
+  function generateYearGrid() {
+    var grid = [];
+    var startYear = Math.floor(selectedYear/10) * 10 - 1;
+
+    for (var i=0; i<3; i++) {
+      var row = [];
+      for (var j=0; j<4; j++) {
+        row[j] = startYear;
+        startYear++;
+      }
+      grid.push(row);
+    }
+
+    return grid;
+  }
+
+  var grid = generateYearGrid();
+
+  return (
+    <>
+      {grid.map((row, rowIndex) => {
+          return (
+            <div key={rowIndex} className="row">
+              {row.map((year, yearIndex) => {
+                return (
+                  <div key={yearIndex} className={`year${year === selectedYear ? ' selected': ''}`} onClick={() => clickYearHandler(year)}>{year}</div>
+                )
+              })}
+            </div>
+          )
+      })}
+    </>
+  )
+}
+
+YearView.propTypes = {
+  yearState: PropTypes.object,
+  typeState: PropTypes.object
+}
+export default YearView;
