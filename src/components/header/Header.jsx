01d117ab635658fd6139beeed6ed9984a7630f30
Grace Mora <autobots00019@gmail.com>
2024-06-13 03:47:09 +0800

    chore: dynamic header rendering

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,36 +2,62 @@ import { MONTHS } from '../../data/months';
 import PropTypes from 'prop-types';
 import './Header.css';
 
-function Header({year, month, modifyMonth, modifyYear}) {
+function Header({ monthState, yearState, typeState}) {
+  const { selectedMonth, setSelectedMonth } = monthState;
+  const { selectedYear, setSelectedYear } = yearState;
+  const { headerType, setHeaderType } = typeState;
 
   function clickNavButtonHandler(num) {
     var computedMonth, computedYear = 0;
     if (num > 0) {
-      computedMonth = month === 11 ? 0 : month+1;
-      computedYear = month === 11 ? year+1 : year;
+      computedMonth = selectedMonth === 11 ? 0 : selectedMonth+1;
+      computedYear = selectedMonth === 11 ? selectedYear+1 : selectedYear;
     } else if (num < 0) {
-      computedMonth = month === 0 ? 11 : month-1;
-      computedYear = month === 0 ? year-1:year;
+      computedMonth = selectedMonth === 0 ? 11 : selectedMonth-1;
+      computedYear = selectedMonth === 0 ? selectedYear-1: selectedYear;
     }
 
-    modifyMonth(computedMonth);
-    modifyYear(computedYear);
+    setSelectedMonth(computedMonth);
+    setSelectedYear(computedYear);
+  }
+
+  function renderHeader() {
+    switch (headerType) {
+      case 2:
+        return `${selectedYear}`
+      case 3:
+        return `${Math.floor(selectedYear/10)*10-1}-${Math.ceil(selectedYear/10)*10}`
+      default:
+        return `${MONTHS[selectedMonth].fullName} ${selectedYear}`;
+    } 
+  }
+
+  function clickHeader() {
+    var type = headerType;
+    if (headerType < 3) {
+      type++;
+    } else {
+      type = 1;
+    }
+
+    setHeaderType(type);
   }
   
   return (
     <div className="row month-year-header">
       <div className="nav-btn" onClick={() => clickNavButtonHandler(-1)}>&lt;</div>
-      <div className="month-year">{`${MONTHS[month].fullName} ${year}`} </div>
+      <div className="month-year" onClick={() => clickHeader()}>
+        {renderHeader()} 
+      </div>
       <div className="nav-btn" onClick={() => clickNavButtonHandler(1)}>&gt;</div>
     </div>
   )
 }
 
 Header.propTypes = {
-  year: PropTypes.number,
-  month: PropTypes.number,
-  modifyMonth: PropTypes.func,
-  modifyYear: PropTypes.func
+  monthState: PropTypes.object,
+  yearState: PropTypes.object,
+  typeState: PropTypes.object
 }
 
 export default Header;
